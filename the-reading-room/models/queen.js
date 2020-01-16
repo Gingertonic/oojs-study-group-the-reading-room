4d9c5be5c58e6bcfce26897c951cdbc2bac701b2
Gingertonic <bethmschofield@gmail.com>
2020-01-16 15:46:04 +0000

    start Queen class

---
+++ the-reading-room/models/queen.js
@@ -0,0 +1,8 @@
+class Queen {
+    constructor(attrs){
+        this.id = attrs.id,
+        this.name = attrs.name,
+        this.quote = attrs.quote,
+        this.imageUrl = attrs.image_url
+    }
+}
